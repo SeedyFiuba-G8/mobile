38dd28f4a3eb2d0af812fb339735f74901dfdea1
Nicolas Aguerre <aguerre31@gmail.com>
2021-07-02 17:55:00 -0300

    [ADD] Adds filter bar to My Projects screen

--- src/screens/MyProjectsScreen.tsx
+++ src/screens/MyProjectsScreen.tsx
@@ -3,6 +3,8 @@ import { StyleSheet, View } from 'react-native';
 
 // Components
 import ProjectList from '../components/Project/ProjectList';
+import { Text, Avatar } from 'react-native-paper';
+import { Picker } from '@react-native-picker/picker';
 
 // Navigation
 import { MaterialTopTabBarProps } from '@react-navigation/material-top-tabs';
@@ -16,6 +18,9 @@ import { getUserProjects } from '../api/projectsApi';
 
 // Hooks
 import { useSelector } from 'react-redux';
+
+// Constant
+import colors from '../constants/colors';
 
 export default function DashboardScreen({
     navigation,
@@ -31,18 +36,43 @@ export default function DashboardScreen({
         setRefreshing(false);
     };
 
+    const [filter, setFilter] = React.useState('all');
+
     useEffect(() => {
         onRefresh();
     }, []);
 
     return (
-        <View style={styles.container}>
-            <ProjectList
-                refreshing={refreshing}
-                onRefresh={onRefresh}
-                projects={projects}
-            />
-        </View>
+        <>
+            <View style={styles.filterBar}>
+                <Avatar.Icon
+                    icon='filter'
+                    size={25}
+                    style={styles.icon}
+                    color={colors.darkGrey}
+                />
+                <Text style={styles.filterText}>View:</Text>
+                <Picker
+                    style={styles.categorySelector}
+                    selectedValue={filter}
+                    onValueChange={(itemValue, itemIndex) =>
+                        setFilter(itemValue)
+                    }
+                    mode='dropdown'
+                >
+                    <Picker.Item label='All' value='all' />
+                    <Picker.Item label='Published' value='published' />
+                    <Picker.Item label='Draft' value='draft' />
+                </Picker>
+            </View>
+            <View style={styles.container}>
+                <ProjectList
+                    refreshing={refreshing}
+                    onRefresh={onRefresh}
+                    projects={projects}
+                />
+            </View>
+        </>
     );
 }
 
@@ -56,8 +86,29 @@ const styles = StyleSheet.create({
         flex: 1,
         alignSelf: 'stretch',
     },
-    title: {
-        fontSize: 20,
+    filterText: {
+        fontSize: 18,
         fontWeight: 'bold',
+        color: colors.darkGrey,
+    },
+    filterBar: {
+        marginHorizontal: 20,
+        marginTop: 10,
+        marginBottom: 0,
+        backgroundColor: '#ddd',
+        alignSelf: 'stretch',
+        borderRadius: 5,
+        padding: 5,
+        alignItems: 'center',
+        flexDirection: 'row',
+        justifyContent: 'center',
+    },
+    categorySelector: {
+        alignSelf: 'stretch',
+        flex: 1,
+        height: 25,
+    },
+    icon: {
+        backgroundColor: 'transparent',
     },
 });
